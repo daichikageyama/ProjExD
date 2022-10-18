c5b67b7b5df72955973c159a5a1b0ac4558e177d
daichi <c0a200413e@edu.teu.ac.jp>
2022-10-18 17:28:47 +0900

    課題03

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import maze_maker as mm
 
+from tkinter import messagebox
+
 def key_down(event):
     global key
     key = event.keysym
@@ -20,6 +22,9 @@ def main_proc():
         mx -= 1
     if key == "Right":
         mx += 1
+    
+
+
     if maze_lst[my][mx] == 0:
         cx, cy = mx*100+50, my*100+50
     else:
@@ -38,22 +43,29 @@ def main_proc():
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
     
+def click_close():
+    if messagebox.askokcancel("確認", "本当に閉じていいですか？"):
+        root.destroy()
+    
+    
 
 
  
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷路")
-    canv = tk.Canvas(width=1500, height=900, bg="black")
+    root.title("10秒迷路")
+    canv = tk.Canvas(width=1500, height=900, bg="pink")
     canv.pack()
     maze_lst = mm.make_maze(15, 9)
     mm.show_maze(canv, maze_lst)
     
     print(maze_lst)
-    tori = tk.PhotoImage(file="fig/5.png")
+    tori = tk.PhotoImage(file="fig/10.png")
+    knife = tk.PhotoImage(file="fig/knife.png")
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50
     canv.create_image(cx, cy, image=tori, tag="tori")
+    canv.create_image(1450, 750, image=knife, tag="knife")
     
     key = ""
     
@@ -61,7 +73,8 @@ if __name__ == "__main__":
     root.bind("<KeyRelease>", key_up)
     
     main_proc()
-    
+    root.protocol("WM_DELETE_WINDOW", click_close)
+    root.after(10000, root.destroy)
     root.mainloop()
     
     
