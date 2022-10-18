81bbd240380f9df11f00f791199801ce82b8172c
daichi <c0a200413e@edu.teu.ac.jp>
2022-10-18 15:33:29 +0900

    練習③だと

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,43 @@
 import tkinter as tk
+import maze_maker as mm
+
+def key_down(event):
+    global key
+    key = event.keysym
+    
+def key_up(event):
+    global key
+    key = ""
+    
+def main_proc():
+    global mx, my
+    global cx, cy
+    if key == "Up":
+        my -= 1
+    if key == "Down":
+        my += 1
+    if key == "Left":
+        mx -= 1
+    if key == "Right":
+        mx += 1
+    if maze_lst[my][mx] == 0:
+        cx, cy = mx*100+50, my*100+50
+    else:
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1
+        
+        
+        
+        
+    canv.coords("tori", cx, cy)
+    root.after(100, main_proc)
+    
 
 
  
@@ -7,7 +46,22 @@ if __name__ == "__main__":
     root.title("迷路")
     canv = tk.Canvas(width=1500, height=900, bg="black")
     canv.pack()
+    maze_lst = mm.make_maze(15, 9)
+    mm.show_maze(canv, maze_lst)
+    
+    print(maze_lst)
     tori = tk.PhotoImage(file="fig/5.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canv.create_image(cx, cy, image=tori, tag="tori")
+    
+    key = ""
+    
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
+    
+    main_proc()
+    
     root.mainloop()
+    
+    
